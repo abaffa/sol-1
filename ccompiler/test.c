df7d74d15bb4481bf215a1835bf6b1baa9cfa189
Paulo Constantino <pconst167@gmail.com>
2023-01-29 17:15:30 +0000

    cc

--- ccompiler/test.c
+++ ccompiler/test.c
@@ -3,12 +3,21 @@
 enum e1 {E1, E2, E3};
 enum e2 {hello, world, haha};
 
-void main(void) {
-  int i;
+char *nl = "\n\r";
+int i;
 
-  i = haha;
+void main(void) {
+
   do{
     i = i + 1;
+    asm{
+      mov a, $i
+      call print_u16d
+
+      mov a, $nl
+      mov d, a
+      call puts
+    }
   } while(i < 10);
 
   return;
