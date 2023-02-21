d99f447ffa3e15d009e223cc419f8935655deb2f
Paulo Constantino <pconst167@gmail.com>
2023-02-21 01:11:00 +0000

    added notes.txt

--- software/mkbin.s
+++ software/mkbin.s
@@ -10,12 +10,15 @@ cmd_mkbin:
 	mov a, 0
 	mov [prog], a
 	call get_token
+	mov d, s_prompt
+	call puts
 	mov d, tokstr
 	mov al, 6
 	syscall sys_fileio
 
 	syscall sys_terminate_proc
 
+s_prompt: .db "% ", 0
 
 .include "token.asm"
 .include "stdio.asm"
