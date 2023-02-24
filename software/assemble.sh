9d0e8e2d4458e1d371accab9d55768bc02d2dcb4
Paulo Constantino <pconst167@gmail.com>
2023-02-24 18:45:58 +0000

    cc

--- software/assemble.sh
+++ software/assemble.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
 
-wine tasm -1 -b kernel.s
-wine tasm -1 -b shell.s
+wine tasm -1 -b kernel.s obj/kernel.obj
+wine tasm -1 -b shell.s lst/kernel.lst
 
 for FILE in *.s; do
+	f=$(echo $FILE | sed 's/.s//g')
 	printf "\n$FILE\n"
-	wine tasm -1 -b $FILE
+	wine tasm -1 -b $FILE obj/$f.obj lst/$f.lst
 done
 
 
 printf "\n$1\n"
-wine tasm -1 -b $1.s
+wine tasm -1 -b $1.s obj/$1.obj lst/$1.lst
 
-xxd -ps $1.obj | tr -d '\n' | xclip -selection c
+xxd -ps obj/$1.obj | tr -d '\n' | xclip -selection c
 printf "\n> binary file copied to clipboard.\n"
