2d8b789eebf88544acab28c78de4d0e38970a1b5
Paulo Constantino <pconst167@gmail.com>
2023-03-17 04:08:33 +0000

    cc

--- ccompiler/test.c
+++ ccompiler/test.c
@@ -1,19 +1,10 @@
 #inc_asm "lib/stdio.asm"
 
-void bubble_sort(char arr[1], int n) {
-    int i, j;
-    char temp;
 
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    return;
+int main() {
+  char s;
+
+  return 0;
 }
 
 void _gets(char *s){
@@ -35,27 +26,6 @@ int _strlen(char str[1]) {
     
     return length;
 }
-int main() {
-    int n, i;
-
-    char s[100];
-    print("Enter the elements of the array as a string: ");
-    _gets(s);
-    print("OK.\n");
-
-    n = _strlen(s);
-    print("Now sorting...\n");
-    bubble_sort(s, n);
-
-    print("Sorted array: ");
-    for (i = 0; i < n; i++) {
-        _putchar(s[i]);
-    }
-    print("\n");
-
-    return 0;
-}
-
 
 void print_num(int num) {
   char digits[5];
@@ -76,24 +46,6 @@ void print_num(int num) {
       _putchar(digits[i]);
   }
   return;
-}
-
-int integer_square_root(int n) {
-    if (n <= 1) {
-        return n;
-    }
-
-    int x;
-    int y;
-    x = n;
-    y = (x + n / x) / 2;
-
-    while (y < x) {
-        x = y;
-        y = (x + n / x) / 2;
-    }
-
-    return x;
 }
 
 void _putchar(char c){
