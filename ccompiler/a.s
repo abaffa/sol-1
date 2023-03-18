871ebde0d2b39d13ada3a38b63645b0a17333c51
Paulo Constantino <pconst167@gmail.com>
2023-03-18 01:54:04 +0000

    cc

--- ccompiler/a.s
+++ ccompiler/a.s
@@ -7,6 +7,7 @@
 main:
   push bp
   mov bp, sp
+  sub sp, 2 ; i
   mov b, [__grid] ; grid
   push a
   mov d, b
@@ -107,15 +108,337 @@ main:
   mov b, 1; CONDUCTOR
   pop d
   mov [d], b
-_while1_cond:
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 5
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 10
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 6
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 10
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 2; ELECTRON_HEAD
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 7
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 10
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 6
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 11
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 3; ELECTRON_TAIL
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 6
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 12
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+_for1_init:
+  mov b, 8
+  push a
+  mov a, b
+  mov [bp + -1], a ; i
+  pop a
+_for1_cond:
+  mov b, [bp + -1] ; i
+  push a
+  mov a, b
+  mov b, 14
+  cmp a, b
+  lodflgs
+  mov bl, al
+  mov g, a
+  shr al, 3
+  shr bl, 2
+  and bl, %00000001
+  xor al, bl
+  mov b, g
+  and bl, %00000001
+  or al, bl ; <= (signed)
+  mov ah, 0
+  mov b, a
+  pop a
+  cmp b, 0
+  je _for1_exit
+_for1_block:
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 7
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, [bp + -1] ; i
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+_for1_update:
+  mov b, [bp + -1] ; i
+  push a
+  mov a, b
+  inc b
+  push a
+  mov a, b
+  mov [bp + -1], a ; i
+  pop a
+  mov b, a
+  pop a
+  jmp _for1_cond
+_for1_exit:
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 7
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 15
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 6
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 15
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 3; ELECTRON_TAIL
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 8
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 15
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 3; ELECTRON_TAIL
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 6
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 16
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 8
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, 16
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+_for2_init:
+  mov b, 17
+  push a
+  mov a, b
+  mov [bp + -1], a ; i
+  pop a
+_for2_cond:
+  mov b, [bp + -1] ; i
+  push a
+  mov a, b
+  mov b, 25
+  cmp a, b
+  lodflgs
+  mov bl, al
+  mov g, a
+  shr al, 3
+  shr bl, 2
+  and bl, %00000001
+  xor al, bl
+  mov b, g
+  and bl, %00000001
+  or al, bl ; <= (signed)
+  mov ah, 0
+  mov b, a
+  pop a
+  cmp b, 0
+  je _for2_exit
+_for2_block:
+  mov b, [__grid] ; grid
+  push a
+  mov d, b
+  push d
+  mov b, 7
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, [bp + -1] ; i
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+  mov b, 1; CONDUCTOR
+  pop d
+  mov [d], b
+_for2_update:
+  mov b, [bp + -1] ; i
+  push a
+  mov a, b
+  inc b
+  push a
+  mov a, b
+  mov [bp + -1], a ; i
+  pop a
+  mov b, a
+  pop a
+  jmp _for2_cond
+_for2_exit:
+_while3_cond:
   mov b, 1
   cmp b, 0
-  je _while1_exit
-_while1_block:
+  je _while3_exit
+_while3_block:
   call print_grid
   call iterate
-  jmp _while1_cond
-_while1_exit:
+  jmp _while3_cond
+_while3_exit:
   mov b, 0
   leave
   syscall sys_terminate_proc
@@ -123,10 +446,10 @@ _while1_exit:
 print_grid:
   push bp
   mov bp, sp
-_for2_init:
+_for4_init:
   mov b, 0
   mov [__y], b
-_for2_cond:
+_for4_cond:
   mov b, [__y] ; y
   push a
   mov a, b
@@ -142,12 +465,12 @@ _for2_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for2_exit
-_for2_block:
-_for3_init:
+  je _for4_exit
+_for4_block:
+_for5_init:
   mov b, 0
   mov [__x], b
-_for3_cond:
+_for5_cond:
   mov b, [__x] ; x
   push a
   mov a, b
@@ -163,9 +486,9 @@ _for3_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for3_exit
-_for3_block:
-_switch4_expr:
+  je _for5_exit
+_for5_block:
+_switch6_expr:
   mov b, [__grid] ; grid
   push a
   mov d, b
@@ -183,63 +506,63 @@ _switch4_expr:
   add d, b
   pop a
   mov b, [d]
-_switch4_comparisons:
+_switch6_comparisons:
   cmp b, 0
-  je _switch4_case0
+  je _switch6_case0
   cmp b, 1
-  je _switch4_case1
+  je _switch6_case1
   cmp b, 2
-  je _switch4_case2
+  je _switch6_case2
   cmp b, 3
-  je _switch4_case3
-_switch4_case0:
+  je _switch6_case3
+_switch6_case0:
   mov b, $20
   mov [__c], bl
-  jmp _switch4_exit ; case break
-_switch4_case1:
-  mov b, $2e
+  jmp _switch6_exit ; case break
+_switch6_case1:
+  mov b, $2a
   mov [__c], bl
-  jmp _switch4_exit ; case break
-_switch4_case2:
-  mov b, $40
+  jmp _switch6_exit ; case break
+_switch6_case2:
+  mov b, $48
   mov [__c], bl
-  jmp _switch4_exit ; case break
-_switch4_case3:
-  mov b, $23
+  jmp _switch6_exit ; case break
+_switch6_case3:
+  mov b, $54
   mov [__c], bl
-  jmp _switch4_exit ; case break
-_switch4_exit:
+  jmp _switch6_exit ; case break
+_switch6_exit:
   mov bl, [__c] ; c
   mov bh, 0
   push bl
   call _putchar
   add sp, 1
-_for3_update:
+_for5_update:
   mov b, [__x] ; x
   inc b
   mov [__x], b
-  jmp _for3_cond
-_for3_exit:
+  jmp _for5_cond
+_for5_exit:
   mov b, $a
   push bl
   call _putchar
   add sp, 1
-_for2_update:
+_for4_update:
   mov b, [__y] ; y
   inc b
   mov [__y], b
-  jmp _for2_cond
-_for2_exit:
+  jmp _for4_cond
+_for4_exit:
   leave
   ret
 
 iterate:
   push bp
   mov bp, sp
-_for5_init:
+_for7_init:
   mov b, 0
   mov [__y], b
-_for5_cond:
+_for7_cond:
   mov b, [__y] ; y
   push a
   mov a, b
@@ -255,12 +578,12 @@ _for5_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for5_exit
-_for5_block:
-_for6_init:
+  je _for7_exit
+_for7_block:
+_for8_init:
   mov b, 0
   mov [__x], b
-_for6_cond:
+_for8_cond:
   mov b, [__x] ; x
   push a
   mov a, b
@@ -276,15 +599,15 @@ _for6_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for6_exit
-_for6_block:
+  je _for8_exit
+_for8_block:
   mov b, 0
   mov [__head_count], b
-_for7_init:
+_for9_init:
   mov b, 1
   neg b
   mov [__dy], b
-_for7_cond:
+_for9_cond:
   mov b, [__dy] ; dy
   push a
   mov a, b
@@ -304,13 +627,13 @@ _for7_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for7_exit
-_for7_block:
-_for8_init:
+  je _for9_exit
+_for9_block:
+_for10_init:
   mov b, 1
   neg b
   mov [__dx], b
-_for8_cond:
+_for10_cond:
   mov b, [__dx] ; dx
   push a
   mov a, b
@@ -330,9 +653,9 @@ _for8_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for8_exit
-_for8_block:
-_if9_cond:
+  je _for10_exit
+_for10_block:
+_if11_cond:
   mov b, [__dx] ; dx
   push a
   mov a, b
@@ -366,11 +689,11 @@ _if9_cond:
   mov bh, 0
   pop al
   cmp b, 0
-  je _if9_exit
-_if9_true:
-  jmp _for8_update ; for continue
-  jmp _if9_exit
-_if9_exit:
+  je _if11_exit
+_if11_true:
+  jmp _for10_update ; for continue
+  jmp _if11_exit
+_if11_exit:
   mov b, [__x] ; x
   push a
   mov a, b
@@ -387,7 +710,7 @@ _if9_exit:
   mov b, a
   pop a
   mov [__ny], b
-_if10_cond:
+_if12_cond:
   mov b, [__nx] ; nx
   push a
   mov a, b
@@ -521,8 +844,8 @@ _if10_cond:
   mov bh, 0
   pop al
   cmp b, 0
-  je _if10_exit
-_if10_true:
+  je _if12_exit
+_if12_true:
   mov b, [__head_count] ; head_count
   push a
   mov a, b
@@ -530,9 +853,9 @@ _if10_true:
   mov [__head_count], b
   mov b, a
   pop a
-  jmp _if10_exit
-_if10_exit:
-_for8_update:
+  jmp _if12_exit
+_if12_exit:
+_for10_update:
   mov b, [__dx] ; dx
   push a
   mov a, b
@@ -540,9 +863,9 @@ _for8_update:
   mov [__dx], b
   mov b, a
   pop a
-  jmp _for8_cond
-_for8_exit:
-_for7_update:
+  jmp _for10_cond
+_for10_exit:
+_for9_update:
   mov b, [__dy] ; dy
   push a
   mov a, b
@@ -550,9 +873,9 @@ _for7_update:
   mov [__dy], b
   mov b, a
   pop a
-  jmp _for7_cond
-_for7_exit:
-_switch11_expr:
+  jmp _for9_cond
+_for9_exit:
+_switch13_expr:
   mov b, [__grid] ; grid
   push a
   mov d, b
@@ -570,16 +893,16 @@ _switch11_expr:
   add d, b
   pop a
   mov b, [d]
-_switch11_comparisons:
+_switch13_comparisons:
   cmp b, 0
-  je _switch11_case0
+  je _switch13_case0
   cmp b, 1
-  je _switch11_case1
+  je _switch13_case1
   cmp b, 2
-  je _switch11_case2
+  je _switch13_case2
   cmp b, 3
-  je _switch11_case3
-_switch11_case0:
+  je _switch13_case3
+_switch13_case0:
   mov b, [__new_grid] ; new_grid
   push a
   mov d, b
@@ -600,9 +923,26 @@ _switch11_case0:
   mov b, 0; EMPTY
   pop d
   mov [d], b
-  jmp _switch11_exit ; case break
-_switch11_case1:
-_if12_cond:
+  jmp _switch13_exit ; case break
+_switch13_case1:
+  mov b, [__new_grid] ; new_grid
+  push a
+  mov d, b
+  push d
+  mov b, [__y] ; y
+  pop d
+  mov a, 80
+  mul a, b
+  add d, b
+  push d
+  mov b, [__x] ; x
+  pop d
+  mov a, 2
+  mul a, b
+  add d, b
+  pop a
+  push d
+_ternary14_cond:
   mov b, [__head_count] ; head_count
   push a
   mov a, b
@@ -629,53 +969,17 @@ _if12_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _if12_else
-_if12_true:
-  mov b, [__new_grid] ; new_grid
-  push a
-  mov d, b
-  push d
-  mov b, [__y] ; y
-  pop d
-  mov a, 80
-  mul a, b
-  add d, b
-  push d
-  mov b, [__x] ; x
-  pop d
-  mov a, 2
-  mul a, b
-  add d, b
-  pop a
-  push d
+  je _ternary14_false
+_ternary14_true:
   mov b, 2; ELECTRON_HEAD
-  pop d
-  mov [d], b
-  jmp _if12_exit
-_if12_else:
-  mov b, [__new_grid] ; new_grid
-  push a
-  mov d, b
-  push d
-  mov b, [__y] ; y
-  pop d
-  mov a, 80
-  mul a, b
-  add d, b
-  push d
-  mov b, [__x] ; x
-  pop d
-  mov a, 2
-  mul a, b
-  add d, b
-  pop a
-  push d
+  jmp _ternary14_exit
+_ternary14_false:
   mov b, 1; CONDUCTOR
+_ternary14_exit:
   pop d
   mov [d], b
-_if12_exit:
-  jmp _switch11_exit ; case break
-_switch11_case2:
+  jmp _switch13_exit ; case break
+_switch13_case2:
   mov b, [__new_grid] ; new_grid
   push a
   mov d, b
@@ -696,8 +1000,8 @@ _switch11_case2:
   mov b, 3; ELECTRON_TAIL
   pop d
   mov [d], b
-  jmp _switch11_exit ; case break
-_switch11_case3:
+  jmp _switch13_exit ; case break
+_switch13_case3:
   mov b, [__new_grid] ; new_grid
   push a
   mov d, b
@@ -718,24 +1022,24 @@ _switch11_case3:
   mov b, 1; CONDUCTOR
   pop d
   mov [d], b
-  jmp _switch11_exit ; case break
-_switch11_exit:
-_for6_update:
+  jmp _switch13_exit ; case break
+_switch13_exit:
+_for8_update:
   mov b, [__x] ; x
   inc b
   mov [__x], b
-  jmp _for6_cond
-_for6_exit:
-_for5_update:
+  jmp _for8_cond
+_for8_exit:
+_for7_update:
   mov b, [__y] ; y
   inc b
   mov [__y], b
-  jmp _for5_cond
-_for5_exit:
-_for13_init:
+  jmp _for7_cond
+_for7_exit:
+_for15_init:
   mov b, 0
   mov [__y], b
-_for13_cond:
+_for15_cond:
   mov b, [__y] ; y
   push a
   mov a, b
@@ -751,12 +1055,12 @@ _for13_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for13_exit
-_for13_block:
-_for14_init:
+  je _for15_exit
+_for15_block:
+_for16_init:
   mov b, 0
   mov [__x], b
-_for14_cond:
+_for16_cond:
   mov b, [__x] ; x
   push a
   mov a, b
@@ -772,8 +1076,8 @@ _for14_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _for14_exit
-_for14_block:
+  je _for16_exit
+_for16_block:
   mov b, [__grid] ; grid
   push a
   mov d, b
@@ -810,18 +1114,18 @@ _for14_block:
   mov b, [d]
   pop d
   mov [d], b
-_for14_update:
+_for16_update:
   mov b, [__x] ; x
   inc b
   mov [__x], b
-  jmp _for14_cond
-_for14_exit:
-_for13_update:
+  jmp _for16_cond
+_for16_exit:
+_for15_update:
   mov b, [__y] ; y
   inc b
   mov [__y], b
-  jmp _for13_cond
-_for13_exit:
+  jmp _for15_cond
+_for15_exit:
   leave
   ret
 
@@ -848,7 +1152,7 @@ print_num:
   mov a, b
   mov [bp + -6], a ; i
   pop a
-_if15_cond:
+_if17_cond:
   mov b, [bp + 5] ; num
   push a
   mov a, b
@@ -860,17 +1164,17 @@ _if15_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _if15_exit
-_if15_true:
+  je _if17_exit
+_if17_true:
   mov b, $30
   push bl
   call _putchar
   add sp, 1
   leave
   ret
-  jmp _if15_exit
-_if15_exit:
-_while16_cond:
+  jmp _if17_exit
+_if17_exit:
+_while18_cond:
   mov b, [bp + 5] ; num
   push a
   mov a, b
@@ -891,8 +1195,8 @@ _while16_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _while16_exit
-_while16_block:
+  je _while18_exit
+_while18_block:
   lea d, [bp + -4] ; digits beginning on the stack
   mov b, d
   push a
@@ -942,9 +1246,9 @@ _while16_block:
   pop a
   mov b, a
   pop a
-  jmp _while16_cond
-_while16_exit:
-_while17_cond:
+  jmp _while18_cond
+_while18_exit:
+_while19_cond:
   mov b, [bp + -6] ; i
   push a
   mov a, b
@@ -965,8 +1269,8 @@ _while17_cond:
   mov b, a
   pop a
   cmp b, 0
-  je _while17_exit
-_while17_block:
+  je _while19_exit
+_while19_block:
   mov b, [bp + -6] ; i
   push a
   mov a, b
@@ -993,8 +1297,8 @@ _while17_block:
   push bl
   call _putchar
   add sp, 1
-  jmp _while17_cond
-_while17_exit:
+  jmp _while19_cond
+_while19_exit:
   leave
   ret
 ; --- END TEXT BLOCK
--- ccompiler/wireworld.c
+++ ccompiler/wireworld.c
@@ -19,9 +19,9 @@ void print_grid() {
         for (x = 0; x < 40; ++x) {
             switch (grid[y][x]) {
                 case EMPTY: c = ' '; break;
-                case CONDUCTOR: c = '.'; break;
-                case ELECTRON_HEAD: c = '@'; break;
-                case ELECTRON_TAIL: c = '#'; break;
+                case CONDUCTOR: c = '*'; break;
+                case ELECTRON_HEAD: c = 'H'; break;
+                case ELECTRON_TAIL: c = 'T'; break;
             }
             _putchar(c);
         }
@@ -47,13 +47,7 @@ void iterate(){
             
             switch (grid[y][x]) {
                 case EMPTY: new_grid[y][x] = EMPTY; break;
-                case CONDUCTOR:
-                     
-                    if(head_count == 1 || head_count == 2)
-                        new_grid[y][x] = ELECTRON_HEAD;
-                    else
-                        new_grid[y][x] = CONDUCTOR;
-                    break;
+                case CONDUCTOR: new_grid[y][x] = (head_count == 1 || head_count == 2) ? ELECTRON_HEAD : CONDUCTOR; break;
                 case ELECTRON_HEAD: new_grid[y][x] = ELECTRON_TAIL; break;
                 case ELECTRON_TAIL: new_grid[y][x] = CONDUCTOR; break;
             }
@@ -69,12 +63,37 @@ void iterate(){
 }
 
 int main() {
-    // Add an oscillator
-    grid[5][5] = CONDUCTOR;
-    grid[6][5] = ELECTRON_HEAD;
-    grid[7][5] = CONDUCTOR;
-    grid[6][6] = ELECTRON_TAIL;
-    grid[6][7] = CONDUCTOR;
+    int i;
+// Clock signal 1
+grid[5][5] = CONDUCTOR;
+grid[6][5] = ELECTRON_HEAD;
+grid[7][5] = CONDUCTOR;
+grid[6][6] = ELECTRON_TAIL;
+grid[6][7] = CONDUCTOR;
+
+// Clock signal 2
+grid[5][10] = CONDUCTOR;
+grid[6][10] = ELECTRON_HEAD;
+grid[7][10] = CONDUCTOR;
+grid[6][11] = ELECTRON_TAIL;
+grid[6][12] = CONDUCTOR;
+
+// Wires connecting the clock signals to the OR gate
+for (i = 8; i <= 14; i++) {
+    grid[7][i] = CONDUCTOR;
+}
+
+// OR gate (Diode-Resistor)
+grid[7][15] = CONDUCTOR;
+grid[6][15] = ELECTRON_TAIL;
+grid[8][15] = ELECTRON_TAIL;
+grid[6][16] = CONDUCTOR;
+grid[8][16] = CONDUCTOR;
+
+// Output wire from the OR gate
+for (i = 17; i <= 25; i++) {
+    grid[7][i] = CONDUCTOR;
+}
 
 
     while (1) {
