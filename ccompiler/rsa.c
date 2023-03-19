33d2b2c219118556aa04591891865cb294609201
Paulo Constantino <pconst167@gmail.com>
2023-03-19 10:56:11 +0000

    cc

--- ccompiler/rsa.c
+++ ccompiler/rsa.c
@@ -2,37 +2,62 @@
 
 
 int main() {
-    int p = 61, q = 53;
-    int n = p * q;
-    int phi = (p - 1) * (q - 1);
-    int e = find_e(phi);
-    int d = find_d(e, phi);
+    int p, q, n, phi, e, d;
+    p = 61;
+    q = 53;
+    n = p * q;
+    phi = (p - 1) * (q - 1);
+    e = find_e(phi);
+    d = find_d(e, phi);
 
-    printf("Public Key: (%u, %u)\n", n, e);
-    printf("Private Key: (%u, %u)\n", n, d);
+    print("Public Key: (");
+    print_num(n);
+    print(", ");
+    print_num(e);
+    print(")\n");
+
+    print("Private Key: (");
+    print_num(n);
+    print(", ");
+    print_num(d);
+    print(")\n");
 
     char input_str[100];
-    printf("Enter a string: ");
-    fgets(input_str, 100, stdin);
+    print("Enter a string: ");
+    _gets(input_str);
 
     int encrypted_chars[100];
     int encrypted_chars_len = 0;
-    printf("Encrypted text: ");
-    for (int i = 0; input_str[i] != '\0' && input_str[i] != '\n'; i++) {
-        encrypted_chars[i] = mod_exp((int)input_str[i], e, n);
-        printf("%u ", encrypted_chars[i]);
+    print("Encrypted text: ");
+    int i;
+    for (i = 0; input_str[i] != '\0' && input_str[i] != '\n'; i++) {
+        encrypted_chars[i] = mod_exp(input_str[i], e, n);
+        print_num(encrypted_chars[i]);
+        print(" ");
         encrypted_chars_len++;
     }
-    printf("\n");
+    print("\n");
 
-    printf("Decrypted text: ");
-    for (int i = 0; i < encrypted_chars_len; i++) {
-        int decrypted_char = mod_exp(encrypted_chars[i], d, n);
-        printf("%c", (char)decrypted_char);
+    int decrypted_char;
+    char c;
+    print("Decrypted text: ");
+    for (i = 0; i < encrypted_chars_len; i++) {
+        decrypted_char = mod_exp(encrypted_chars[i], d, n);
+        c = decrypted_char;
+        print(&c);
     }
-    printf("\n");
+    print("\n");
 
     return 0;
+}
+
+void _gets(char *s){
+  asm{
+    mov a, @s
+    mov d, a
+    call gets
+  }
+  return;
 }
 
 int gcd(int a, int b) {
@@ -48,12 +73,45 @@ int mod_exp(int base, int exp, int mod) {
         if (exp & 1) {
             result = (result * base) % mod;
         }
-        exp >>= 1;
+        exp = exp >> 1;
         base = (base * base) % mod;
     }
     return result;
 }
 
+void _putchar(char c){
+  asm{
+    mov al, @c
+    mov ah, al
+    call putchar
+  }
+}
+void print(char *s){
+    asm{
+        mov a, @s
+        mov d, a
+        call puts
+    }
+}
+void print_num(int num) {
+  char digits[5];
+  int i;
+  i = 0;
+  if(num == 0){
+    _putchar('0');
+    return;
+  }
+  while (num > 0) {
+      digits[i] = '0' + (num % 10);
+      num = num / 10;
+      i++;
+  }
+  // Print the digits in reverse order using putchar()
+  while (i > 0) {
+      i--;
+      _putchar(digits[i]);
+  }
+}
 int find_e(int phi) {
     int e;
     for (e = 2; e < phi; e++) {
